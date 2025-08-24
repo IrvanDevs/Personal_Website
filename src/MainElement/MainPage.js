ab60ef018f6d575249d72bbca9e95d089b0b3a30
irvandevs <irvangt08@gmail.com>
2025-08-24 23:25:16 +0700

    snap fix

--- src/MainElement/MainPage.js
+++ src/MainElement/MainPage.js
@@ -3,7 +3,7 @@ import Menu from '../MainElement/MenuHeader';
 
 function MainPage() {
   return (
-    <div className='Main overflow-x-hidden overflow-y-auto snap-mandatory snap-y scroll-smooth h-screen'>
+    <div className='Main overflow-x-hidden overflow-y-auto scroll-smooth h-screen snap-none md:snap-y md:snap-mandatory'>
       <Menu />
       <Home />
     </div>
